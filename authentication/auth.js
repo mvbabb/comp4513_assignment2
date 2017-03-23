225e98b29250c46a2467e1849b6373e496fe2fe3
Stepw868 <cruessa@gmail.com>
2017-03-23 15:14:52 -0600

    added the basics of mongodb
    currently doesnt do anythingwith mongodb, but it loads some info into it
    and retrieves it back.  Not sure how portable this will be though... not
    sure if it will still have the info on other machines

--- authentication/auth.js
+++ authentication/auth.js
@@ -5,9 +5,12 @@
     -if the app is valid(same as above) then verify the user for access
     -everyone is  an "admin level"
     -submit through the google drive (mbabb668), send a share link to Padma
-    
 
+-username: normal
+-password: normal123
 
+-adminUsername: admin
+-adminPassword: admin123#
 */
 
 
@@ -19,6 +22,8 @@ var path = require('path');
 var app=express();
 var btoa = require('btoa');
 var atob = require('atob');
+//var mongodb = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
 //app.set('port', 3001);
 app.use(express.static(path.join(__dirname)));
 //app.use(express.static("application"));
@@ -27,12 +32,34 @@ app.use(express.static(path.join(__dirname)));
 app.use(bodyParse.urlencoded({extended:false}));
 
 
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
+MongoClient.connect("mongodb://localhost:27017/YLN", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+
+    db.createCollection('users', function(err, collection) {});
+    var collection = db.collection('users');
+    var doc1 = {'hello':'doc1', 'key':'1'};
+    var doc2 = {'hello':'doc2', 'key':'2'};
+    var lotsOfDocs = [{'hello':'doc3', 'key':'4'}, {'hello':'doc4', 'key':'5'}];
+    console.log("before the inserts!");
+    collection.insert(doc1);
+    collection.insert(doc2, {w:1}, function(err, result) {});
+    //collection.insert(lotsOfDocs, {w:1}, function(err, result) {});
+    var testResults = collection.find().each(function(err, doc) {
+      console.log(doc);
+    });
+    //console.log(testResults);
+    db.close();
+  }
+
+});
 
 app.get('/login', function(req,res){
     res.send("<h1 id='test' style='color:red'>hi there: I am in the root of Michael Babb</h1>");
@@ -72,32 +99,32 @@ app.post('/', function(req,res){
     var appContent = JSON.parse(tokens);
     var user_name_node=req.body.uname;
     var password_node=req.body.psw;
-    console.log("Username:" + user_name_node + " Password:" + password_node); 
+    console.log("Username:" + user_name_node + " Password:" + password_node);
     var authResponse = "false";
-    
+
     for(x in jsonContent){
         jsonContent[x].logToken = "false";
     }
-    
-    
+
+
     for (x in jsonContent){
       if (atob(jsonContent[x].user_name) == user_name_node){
           if(password_node == atob(jsonContent[x].password)){
             authResponse = "true";
           }
-          
+
           if(authResponse == "true"){
               jsonContent[x].logToken = "true";
               jsonContent = JSON.stringify(jsonContent);
               reader.writeFileSync("user_info.json",jsonContent);
-                     res.redirect("http://localhost:3001/newsfeed");  
+                     res.redirect("http://localhost:3001/newsfeed");
               }
-          
+
           else{
-               
+
               res.redirect("http://localhost:3001/error");
-                    
-            }        
+
+            }
               }
         };
 
@@ -111,7 +138,7 @@ app.post('/authentication_server', function(req,res){
     var appContent = JSON.parse(tokens);
     var user_name_node=req.body.uname;
     var password_node=req.body.psw;
-    console.log("Username:" + user_name_node + " Password:" + password_node); 
+    console.log("Username:" + user_name_node + " Password:" + password_node);
     var authResponse = "false";
         for(x in jsonContent){
         jsonContent[x].logToken = "false";
@@ -121,19 +148,19 @@ app.post('/authentication_server', function(req,res){
           if(password_node == atob(jsonContent[x].password)){
             authResponse = "true";
           }
-          
+
           if(authResponse == "true" && jsonContent[x].level == "admin"){
               jsonContent[x].logToken = "true";
               jsonContent = JSON.stringify(jsonContent);
               reader.writeFileSync("user_info.json",jsonContent);
-                     res.redirect("http://localhost:3002/secure");  
+                     res.redirect("http://localhost:3002/secure");
               }
-          
+
           else{
-               
+
               res.redirect("http://localhost:3002/");
-                    
-            }        
+
+            }
               }
         };
 
@@ -151,7 +178,7 @@ app.post('/new_user', function(req,res){
         if(jsonContent[x].user_name == req.body.uname)
         cont = "false";
     }
-    
+
     if(cont == true){
     var totalUsers = Object.keys(jsonContent).length + 1;
     jsonContent = JSON.stringify(jsonContent);
@@ -162,13 +189,13 @@ app.post('/new_user', function(req,res){
     //var appContent = JSON.parse(tokens);
     var newUser = "";
     newUser =' , "user' + totalUsers +'": {"user_name":"'+ user_name_node + '","name":"' + actual_name + '","password":"' + password_node + '","token":"coolAssToken", "logToken":"false","level":"regular"}} ';
-         
+
         jsonContent = jsonContent.substr(0,jsonContent.length - 1);
         jsonContent = jsonContent + newUser;
     console.log(jsonContent);
-    console.log("Username:" + user_name_node + " Password:" + password_node); 
+    console.log("Username:" + user_name_node + " Password:" + password_node);
     reader.writeFileSync("user_info.json",jsonContent);
-    
+
     res.redirect("http://localhost:3001/secure");
 
   }else{res.redirect("http://localhost:3001/secure")}});
@@ -187,22 +214,22 @@ app.post('/addApp', function(req,res){
         cont = "false";
         }
     }
-    
+
     if(cont == "true"){
-    
-    
+
+
     jsonContent = JSON.stringify(jsonContent);
     var add_app_name=req.body.add_app_name;
     var add_token_name=req.body.add_token_name;
     var newApp = "";
     newApp =' ,"' + add_app_name +'": {"token":"'+ add_token_name + '"}} ';
-         
+
     jsonContent = jsonContent.substr(0,jsonContent.length - 1);
     jsonContent = jsonContent + newApp;
     console.log(jsonContent);
-    console.log("appName:" + add_app_name + "Token:" + add_token_name); 
+    console.log("appName:" + add_app_name + "Token:" + add_token_name);
     reader.writeFileSync("app_token.json",jsonContent);
-    
+
     res.redirect("http://localhost:3002/secure");
 
   }else{res.redirect("http://localhost:3002/secure");}});
@@ -216,9 +243,9 @@ app.post('/removeApp', function(req,res){
     delete jsonContent[appToDelete];
     console.log(jsonContent);
     jsonContent = JSON.stringify(jsonContent);
-    //console.log("Username:" + user_name_node + " Password:" + password_node); 
+    //console.log("Username:" + user_name_node + " Password:" + password_node);
     reader.writeFileSync("app_token.json",jsonContent);
-    
+
     res.redirect("http://localhost:3002/secure");
 
   });
@@ -250,9 +277,9 @@ app.post('/add_admin', function(req,res){
         cont = "false";
         }
     }
-    
+
     if(cont == "true"){
-    
+
     var totalUsers = Object.keys(jsonContent).length + 1;
     jsonContent = JSON.stringify(jsonContent);
     var user_name_node=btoa(req.body.addAdmin);
@@ -260,21 +287,21 @@ app.post('/add_admin', function(req,res){
     var actual_name = req.body.actualAdminName;
     var level = req.body.level;
     var token = req.body.appToAddUser;
-    
+
     var apps = reader.readFileSync("app_token.json");
     var appContent = JSON.parse(apps);
-    
+
     var usersToken = appContent[token];
         console.log(usersToken.token);
     var newUser = "";
     newUser =' , "user' + totalUsers +'": {"user_name":"'+ user_name_node + '","name":"' + actual_name + '","password":"' + password_node + '","token":"' + usersToken.token + '", "logToken":"false","level":"' + level + '"}} ';
-         
+
         jsonContent = jsonContent.substr(0,jsonContent.length - 1);
         jsonContent = jsonContent + newUser;
     console.log(jsonContent);
-    console.log("Username:" + user_name_node + " Password:" + password_node); 
+    console.log("Username:" + user_name_node + " Password:" + password_node);
     reader.writeFileSync("user_info.json",jsonContent);
-    
+
     res.redirect("http://localhost:3002/secure");
 
   }else{res.redirect("http://localhost:3002/secure");}});
@@ -290,9 +317,9 @@ app.post('/remove_admin', function(req,res){
     delete jsonContent[userToDelete];
     console.log(jsonContent);
     jsonContent = JSON.stringify(jsonContent);
-    //console.log("Username:" + user_name_node + " Password:" + password_node); 
+    //console.log("Username:" + user_name_node + " Password:" + password_node);
     reader.writeFileSync("user_info.json",jsonContent);
-    
+
     res.redirect("http://localhost:3002/secure");
 
   });
@@ -307,9 +334,9 @@ app.post('/modify_admin', function(req,res){
     jsonContent[userToModify].user_name = user;
     console.log(jsonContent);
     jsonContent = JSON.stringify(jsonContent);
-    //console.log("Username:" + user_name_node + " Password:" + password_node); 
+    //console.log("Username:" + user_name_node + " Password:" + password_node);
     reader.writeFileSync("user_info.json",jsonContent);
-    
+
     res.redirect("http://localhost:3002/secure");
 
   });
@@ -320,17 +347,3 @@ var server = app.listen(3002, function(){
     console.log("Server started at http://%s:%s", host, port);
 }
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
