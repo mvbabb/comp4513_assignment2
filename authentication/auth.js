c4fc0bbea150d188a01340c81429003ac050ef0e
Stepw868 <cruessa@gmail.com>
2017-03-25 15:53:10 -0600

    localhost:3002/get-all-info => pulls everything from MongoDB

--- authentication/auth.js
+++ authentication/auth.js
@@ -25,7 +25,7 @@ var app=express();
 var btoa = require('btoa');
 var atob = require('atob');
 //var mongodb = require('mongodb');
-var MongoClient = require('mongodb').MongoClient;
+var mongo = require('mongodb').MongoClient;
 var userDBJSON;
 //app.set('port', 3001);
 app.use(express.static(path.join(__dirname)));
@@ -35,8 +35,10 @@ app.use(express.static(path.join(__dirname)));
 app.use(bodyParse.urlencoded({extended:false}));
 var assert = require('assert');
 var url = "mongodb://localhost:27017/YLN";
-
-var exampleUser= { "_id" : "0100", "user_name" : "user1", "password":"pass",
+// "_id" : "0100",
+//mongo will auto-set the _key field if I dont.
+//if username and password match, save that ID on login
+var exampleUser= {"user_name" : "user1", "password":"pass",
 "name":"Michael", "token":"coolAssToken", "logToken":"true", "level":"admin",
 "feeds" : [
   { "feed_id":"0001", "feed_name":"feed1", "sources": [ "bbc", "buzzfeed", "cnn"] },
@@ -48,6 +50,7 @@ var exampleUser= { "_id" : "0100", "user_name" : "user1", "password":"pass",
 
 
 
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -55,7 +58,7 @@ app.use(function(req, res, next) {
 });
 
 //mongo DB test secion
-MongoClient.connect(url, function(err, db) {
+mongo.connect(url, function(err, db) {
   if(!err) {
     console.log("We are connected");
 
@@ -65,9 +68,8 @@ MongoClient.connect(url, function(err, db) {
     var doc2 = {'hello':'doc2', 'key':'2'};
     var lotsOfDocs = [{'hello':'doc3', 'key':'4'}, {'hello':'doc4', 'key':'5'}];
     console.log("before the inserts!");
-    //collection.insert(doc1);
+    //collection.insert(exampleUser);
     //collection.insert(doc2, {w:1}, function(err, result) {});
-    //asdasd
     //find().each will grab the whole thing
     //find().next will grab one at a time
 
@@ -80,6 +82,26 @@ MongoClient.connect(url, function(err, db) {
   }
 });
 //end mongo test section
+
+app.post('/get-all-data', function(req, res){
+  var resultArray = [];
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var cursor = db.collection('users').find();
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      db.close();
+      console.log(resultArray);
+      res.send(resultArray);
+    });
+  });
+
+});
+
+
+
 
 
 
