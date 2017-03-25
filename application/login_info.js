fa385d2a8f2ca7e0790e162ddd89083e9b79ef64
mvbabb <mbabb668@mtroyal.ca>
2017-03-25 15:23:01 -0600

    login info change

--- application/login_info.js
+++ application/login_info.js
@@ -9,8 +9,13 @@
                           $.post("localhost:3002",{"un":un,"pw":pw},function(data){
                               console.log(data);
                               console.log("Posted the login request");
+                            $.post("localhost:3001/info",{"un":un/*,"pw":pw*/},function(data){
+                              
+                              console.log("Looking for dat data");
                           })
                           
+                          })
+
                           
                           console.log("user:" + un + " pass:" + pw);
                           
