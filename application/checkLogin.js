d5d936085e7cec84f0e92744bc812df90243c0c5
mvbabb <mbabb668@mtroyal.ca>
2017-03-28 10:29:13 -0600

    more cool shit in the navbar

--- application/checkLogin.js
+++ application/checkLogin.js
@@ -6,3 +6,15 @@ if(document.referrer == "http://localhost:3001/newsfeed" || document.referrer ==
 else{
     window.location.replace("http://localhost:3001/");
 }
+
+
+$.ajax({
+                        url: "http://localhost:3002/loggedIn",
+                        dataType: 'json',
+                        type: 'GET',
+                        success: function (data) {
+                            
+                            
+                            
+                        }
+});
