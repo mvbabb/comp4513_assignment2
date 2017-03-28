00f67ce606a8f0e6244ab7bf87792712b468a7d1
srich020 <srich020@mtroyal.ca>
2017-03-28 10:31:08 -0600

    Now I return a user

--- application/app.js
+++ application/app.js
@@ -134,7 +134,8 @@ var options = {
 request(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         // Print out the response body
-        console.log(body)
+        req.session = body;
+		console.log(req.session);
     }
 })
 });
